13510a5669d83825c3c10ee19b8e3d245e084d80
Prince <137284876+imparasparabiya@users.noreply.github.com>
2023-09-01 22:51:46 +0530

    Add files via upload

---
+++ L3n.java
@@ -0,0 +1,24 @@
+public class L3n{
+	public static void main(String ags[]){
+			int a = 1,b = 2,c = 3;
+			
+			if(a>b){
+			      if(a>c){
+				     System.out.println(a+" is Big"); 
+				  }
+				  else{
+				     System.out.println(c+" is Big"); 
+				  }	
+			}
+			else{
+				if(b>c){
+				
+				     System.out.println(b+" is Big"); 
+				}
+				else{
+				
+				     System.out.println(c+" is Big"); 
+				}
+			}
+	}
+}
---
+++ Tebal.java
@@ -0,0 +1,13 @@
+public class Tebal{
+		public static void main(String ard[]){
+			int a=5, i=1;
+			
+			while(i<=10){
+				
+				System.out.println(a+ " * "+i+ " = "+(a*i));
+				i++;
+			}
+			
+		}
+	
+}
